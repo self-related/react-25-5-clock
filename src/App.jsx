2ad0ea5efb4d9a4bbfd74a9f405407bd0542dc26
Nekit <nick.misiura@yandex.ru>
2024-10-21 22:44:37 +0300

    feat: добавлен функционал основного таймера

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 
 function Buttons({ isCounting, changeCountingMode }) {
@@ -16,12 +16,12 @@ function Buttons({ isCounting, changeCountingMode }) {
   );
 }
 
-function Timer({ breakTime, sessionTime, isSession, isCounting}) {
+function Timer({ isSession, timerTime }) {
 
   return (
     <div id='timer' className='center'>
       <p id='timer-label'>{isSession == true ? 'Session' : 'Break'}</p>
-      <div id='time-left'>00:00</div>
+      <div id='time-left'>{timerTime}</div>
     </div>
   );
 }
@@ -51,18 +51,42 @@ function Controls({ breakTime, sessionTime, changeTime }) {
 }
 
 function App() {
-  const [sessionTime, setSessionTime] = useState(0);
-  const [breakTime, setBreakTime] = useState(0);
+  const [sessionTime, setSessionTime] = useState(6000);
+  const [breakTime, setBreakTime] = useState(2000);
   const [isSession, setIsSession] = useState(true);
   const [isCounting, setIsCounting] = useState(false);
+  const [timerTime, setTimerTime] = useState(sessionTime);
+  const isCountingRef = useRef(null);
+
+  const startTimer = async () => {
+    if (!isCounting) { return; }
+    if (breakTime < 1000 || sessionTime < 1000) { return; }
+
+    if (timerTime === 0) {
+      if (isSession) {
+        setTimerTime(breakTime);
+      } else {
+        setTimerTime(sessionTime);
+      }
+      setIsSession(isSession => !isSession);
+      /* sound */
+      return;
+    }
+
+    setTimeout(() => {
+        if (!isCountingRef.current) return;
+      setTimerTime((timerTime) => timerTime - 1000);
+    }, 1000);
+  };
 
   const changeCountingMode = () => {
     setIsCounting( (isCounting) => !isCounting );
+    isCountingRef.current = !isCounting; //для таймера
   };
 
   const changeTime = (timeToChange, changeMode) => {
     if (isCounting) { return; }
-    
+
     const setTime = timeToChange === 'session' ? setSessionTime : setBreakTime;
     if (changeMode === "+") {
       setTime((time) => time + 60_000);
@@ -77,11 +101,13 @@ function App() {
     }
   };
 
+  startTimer();
+
   return (
     <div id="app">
       <h1>25-5 Clock</h1>
       <Controls breakTime={breakTime} sessionTime={sessionTime} changeTime={changeTime}/>
-      <Timer breakTime={breakTime} sessionTime={sessionTime} isSession={isSession} isCounting={isCounting}/>
+      <Timer isSession={isSession} timerTime={timerTime}/>
       <Buttons isCounting={isCounting} changeCountingMode={changeCountingMode}/>
     </div>
   )
